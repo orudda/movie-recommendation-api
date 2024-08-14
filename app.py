bf04e272e117336444220894a449c5caa5246ce2
orudda <othavio.rudda.cc@gmail.com>
2024-08-14 16:54:12 -0300

    requirements updated

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
-import os
 from src.routes.router import main_router
-from src.db import init_db
 
 # Carregar as variáveis de ambiente do arquivo .env
 load_dotenv()
