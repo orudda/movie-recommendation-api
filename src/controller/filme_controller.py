f42ca7b9eaede0d7b249c2540de0677ca361d080
orudda <othavio.rudda.cc@gmail.com>
2024-08-15 07:51:09 -0300

    refactoring: bug on get_filmes error handler removed

--- src/controller/filme_controller.py
+++ src/controller/filme_controller.py
@@ -1,4 +1,3 @@
-from fastapi import HTTPException
 from fastapi.responses import JSONResponse
 from src.service.filme_service import FilmeService, ServiceError
 from sqlalchemy.orm import Session
@@ -9,8 +8,11 @@ class FilmeController:
 
         try:
             return FilmeService.fetch_filmes(db)
-        except ValueError as e:
-            raise HTTPException(status_code=204, detail=str(e))
+        except ServiceError as e:
+            return JSONResponse(
+                content={"error": str(e)},
+                status_code=e.status_code
+            )
         
     
     def get_recommendations(usuario_id: int, db: Session):
